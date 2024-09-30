960f38f9a034931973e4e1a5501a5497cc07b96c
Flávia Munhoz <flavia.helena.munhoz@gmail.com>
2024-09-30 16:53:20 -0300

    add-new-function-python

---
+++ function2.py
@@ -0,0 +1,50 @@
+# cadastrar: opção para adicionar (turma, nome e sobrenome).
+
+
+
+def dados_aluno(): #captar dados do aluno
+    print("Ola\n Cadestre o aluno")
+    nome = input("Digite o nome do aluno: ").upper()
+    sobrenome = input("Digite o sobrenome do aluno: ").upper()
+    aluno = {
+        "nome": nome,
+        "sobrenome": sobrenome #adicionar nota e frequencia( ver se faz funcao ou nao)
+    }
+    return aluno
+
+def escolher_turma():  # Opção de turma 
+    TURMA = input("Informe a que turma o aluno está matriculado\n turma [A] ou turma [B]: ").upper()
+    if TURMA == "A":
+        return "turma-A.txt"  # nome do arquivo para a turma A
+    elif TURMA == "B":
+        return "turma-B.txt"  # nome do arquivo para a turma B
+    else:
+        print("Insira uma turma válida!")  # Mensagem de erro
+        return None  # Retorna nada para entrada errada   
+
+def add_novo_aluno():  # adicionar alunos na lista e salvar
+    aluno = dados_aluno()
+    path_file = escolher_turma() 
+    if path_file:  # Verifica se a turma é válida
+        salvar_novo_aluno(path_file, **aluno)  # Salva o aluno no arquivo correspondente
+        print(f"Aluno adicionado: {aluno}")  # Mensagem de confirmação
+    else:
+        print("Aluno não adicionado devido a erro na seleção da turma.")  # Mensagem de erro
+    print(f"Aluno adicionado: {aluno}")
+    
+def salvar_novo_aluno(path_file, **kwargs):# cria arquivo
+    with open(path_file, 'a') as file:
+        for chave, valor in kwargs.items():
+            file.write(f"\n{chave}: {valor}\n")    
+            
+def add_nota():
+    N1= input("")
+    N2= input("")
+    N3= input("")
+    
+            
+
+
+add_novo_aluno()
+
+    
